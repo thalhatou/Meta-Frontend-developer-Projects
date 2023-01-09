2915411c85132d5059c527ee68dcabfdead5e8fe
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2023-01-09 03:23:57 +0100

    Nested loops

---
+++ learn.js
@@ -0,0 +1,19 @@
+
+// single loop
+for (let first_numb = 0; first_numb < 2; first_numb++) {
+    console.log(first_numb)
+}
+
+for (let second_numb = 0; second_numb < 10; second_numb++) {
+    console.log(second_numb)
+}
+
+// nested loop
+for (let first_numb = 0; first_numb < 2; first_numb++) {
+    for (let second_numb = 0; second_numb < 10; second_numb++) {
+        console.log(`${first_numb},${second_numb}`)
+    }
+}
+
+
+
